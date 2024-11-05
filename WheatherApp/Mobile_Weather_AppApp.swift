ddca1489bea5a9d7503cd6554f66064325a01472
Santosh Sugur <santosh.iosdev@gmail.com>
2024-11-06 00:22:45 +0530

    Added classes and its respective Module s.
    Created Models and its API Services .

---
+++ WheatherApp/Mobile_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Weather_AppApp.swift
+//  Mobile Weather App
+//
+//  Created by Stewart Lynch on 2021-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
