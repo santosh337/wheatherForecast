dc964bd490c5d98bec054d45db0d556b13908594
S <santosh.sugur@ext.icicibank.com>
2024-11-06 00:22:45 +0530

    Added classes and its respective Module s.
    Created Models and its API Services .

--- WheatherApp/ContentView.swift
+++ WheatherApp/ContentView.swift
@@ -1,21 +1,113 @@
 //
 //  ContentView.swift
-//  WheatherApp
+//  Mobile Weather App
 //
-//  Created by ban414876 on 05/11/24.
+//  Created by Stewart Lynch on 2021-01-18.
 //
 
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject  var forecastListVM = ForecastListViewModel()
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        ZStack {
+            NavigationView {
+                VStack {
+                    Picker(selection: $forecastListVM.system, label: Text("System")) {
+                        Text("°C").tag(0)
+                        Text("°F").tag(1)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 100)
+                    .padding(.vertical)
+                    HStack {
+                        TextField("Enter Location", text: $forecastListVM.location,
+                                  onCommit: {
+                                    forecastListVM.getWeatherForecast()
+                                  })
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .overlay (
+                                Button(action: {
+                                    forecastListVM.location = ""
+                                    forecastListVM.getWeatherForecast()
+                                }) {
+                                    Image(systemName: "xmark.circle")
+                                        .foregroundColor(.gray)
+                                }
+                                .padding(.horizontal),
+                                alignment: .trailing
+                            )
+                        Button {
+                            forecastListVM.getWeatherForecast()
+                        } label: {
+                            Image(systemName: "magnifyingglass.circle.fill")
+                                .font(.title3)
+                        }
+                    }
+                    List(forecastListVM.forecasts, id: \.day) { day in
+                            VStack(alignment: .leading) {
+                                Text(day.day)
+                                    .fontWeight(.bold)
+                                HStack(alignment: .center) {
+                                    WebImage(url: day.weatherIconURL)
+                                        .resizable()
+                                        .placeholder {
+                                            Image(systemName: "hourglass")
+                                        }
+                                        .scaledToFit()
+                                        .frame(width: 75)
+                                    VStack(alignment: .leading) {
+                                        Text(day.overview)
+                                            .font(.title2)
+                                        HStack {
+                                            Text(day.high)
+                                            Text(day.low)
+                                        }
+                                        HStack {
+                                            Text(day.clouds)
+                                            Text(day.pop)
+                                        }
+                                        Text(day.humidity)
+                                    }
+                                }
+                            }
+                        }
+                        .listStyle(PlainListStyle())
+                }
+                .padding(.horizontal)
+                .navigationTitle("Mobile Weather")
+                .alert(item: $forecastListVM.appError) { appAlert in
+                    Alert(title: Text("Error"),
+                          message: Text("""
+                            \(appAlert.errorString)
+                            Please try again later!
+                            """
+                            )
+                    
+                    )
+                }
+            }
+            if forecastListVM.isLoading {
+                ZStack {
+                    Color(.white)
+                        .opacity(0.3)
+                        .ignoresSafeArea()
+                    ProgressView("Fetching Weather")
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(.systemBackground))
+                        )
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/ )
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
